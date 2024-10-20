fffab42658d72367a90e20948ed1533a5fafe67f
Sergey Kazarinov <kazarinov092@gmail.com>
2024-10-20 18:14:31 +0500

    feat: add register page

--- src/core/templator/test.html.js
+++
@@ -1,5 +0,0 @@
-export const test = (function () {
-  return `
-<h1>{{test}}</h1>
-`;
-})();
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import Templator from "./core/templator/Temlator";
 import { sum } from "./modules/sum";
 import { login } from "./pages/login/login.tmpl";
+import { register } from './pages/register/register.tmpl';
 
 const root = document.querySelector("#root");
 root.textContent = sum(6, -1).toString();
-const tmpl = new Templator(login);
+const tmpl = new Templator(register);
 
 const context = {
-  title: "Вход",
+  title: "Регистрация",
 };
 
 const renderedTemplate = tmpl.compile(context); // Строка с html-вёрсткой
